b6ee338fde2a16686e707ec4a99dab0db5812e95
CodeDhruv06 <rsdhruv2006@mail.com>
2025-12-07 12:47:31 +0530

    Added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head >
+        <link rel="icon" type="image/jpg" href="/logo.jpg"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
